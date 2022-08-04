f1d72b4c92f8e1cfb53d348893641cef28b813b8
HavliukOlena <109664575+HavliukOlena@users.noreply.github.com>
2022-08-04 08:11:58 +0300

    API Intagrated

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day}   ${hours}:${minutes}`;
+}
 
+function displayTemperature(response) {
+  let realTemperature = document.querySelector("#temperature");
+  realTemperature.innerHTML = Math.round(response.data.main.temp);
+
+  let realCity = document.querySelector("#city");
+  realCity.innerHTML = response.data.name;
+
+  let realDescription = document.querySelector("#description");
+  realDescription.innerHTML = response.data.weather[0].description;
+
+  let realHumidity = document.querySelector("#humidity");
+  realHumidity.innerHTML = response.data.main.humidity;
+
+  let realWind = document.querySelector("#wind");
+  realWind.innerHTML = Math.round(response.data.wind.speed);
+
+  let realDate = document.querySelector("#date");
+  realDate.innerHTML = formatDate(response.data.dt * 1000);
+}
+
+let apiKey = "13503136859e9dffd277dab65a0e3801";
+let city = "Paris";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
