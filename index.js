86a2ff6224eef925e430c53c6c1776edb7dac92d
HavliukOlena <109664575+HavliukOlena@users.noreply.github.com>
2022-08-04 09:26:44 +0300

    added search engine

--- index.js
+++ index.js
@@ -47,9 +47,17 @@ function displayTemperature(response) {
   );
   realIcon.setAttribute("alt", response.data.weather[0].description);
 }
+function search(city) {
+  let apiKey = "13503136859e9dffd277dab65a0e3801";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-let apiKey = "13503136859e9dffd277dab65a0e3801";
-let city = "Athens";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function handleSubmit(event) {
+  event.preventDefault();
+  let realCityInput = document.querySelector("#city-input");
+  search(realCityInput.value);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
