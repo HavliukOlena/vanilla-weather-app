47cd2619f210bb1585d17edd43b79d8800d4a858
HavliukOlena <109664575+HavliukOlena@users.noreply.github.com>
2022-08-11 09:23:14 +0300

    added API forecast real data

--- index.js
+++ index.js
@@ -20,30 +20,49 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day}   ${hours}:${minutes}`;
 }
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
   forecastHTML = `<div class="row">`;
-  let days = ["Thur", "Fri", "Sat"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="row">
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
               <div class="col-2">
-                <div class="weathre-forecast-date">Mon</div>
+                <div class="weathre-forecast-date">${formatDay(
+                  forecastDay.dt
+                )}</div>
                 <img
-                  src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-                  alt="partly_cloudy"
+                  src="https://openweathermap.org/img/wn/${
+                    forecastDay.weather[0].icon
+                  }@2x.png"
+                  alt=""
                   width="45"
                 />
                 <div class="weathre-forecast-temprature">
-                  <span class="weathre-forecast-temprature-max">20°</span>
-                  <span class="weathre-forecast-temprature-min">18°</span>
+                  <span class="weathre-forecast-temprature-max">${Math.round(
+                    forecastDay.temp.max
+                  )}°</span>
+                  <span class="weathre-forecast-temprature-min">${Math.round(
+                    forecastDay.temp.min
+                  )}°</span>
                 </div>
               </div>
-            </div>
+            
   `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
@@ -52,7 +71,8 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   let apiKey = "13503136859e9dffd277dab65a0e3801";
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `http://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  //let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=e754e9cc07f0baead9b1527a7bc67441&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
